f33b36f7b2ae95b423750170a00cbe1e75859f38
Marco Borth <borthmarco@ku.edu>
2019-10-31 13:50:17 -0500

    crosshair created and implemented to project. Also Scene Reloading Script developed.

---
+++ thisprojectneedsaname/Assets/Scripts/RestartGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/**
+*  @class description: RestartGame Contains scene management reloading command.
+*  @libraries cited: UnityEngine.SceneManagement library was used for the Sceen Manager to reload the game scene.
+*/
+public class RestartGame : MonoBehaviour
+{
+    public UnityEngine.UI.Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(Restart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**
+    * @pre Restart method will listen for the button onClick event.
+    * @post Restart method will reload the scene.
+    * @param None.
+    * @return None.
+    */
+    public void Restart()
+    {
+        /**
+        * The line of code for the Restart() function can be found in the Unity Answer Forum: https://answers.unity.com/questions/46918/reload-scene-when-dead.html
+        */
+        Debug.Log("Restart Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
