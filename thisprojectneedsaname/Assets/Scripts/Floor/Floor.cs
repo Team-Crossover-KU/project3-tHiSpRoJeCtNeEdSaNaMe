33a8027f3e8677954dcc2f9f4e89f2d7fc3ceac9
Jack McClure <jack_mcclure@ku.edu>
2019-11-03 20:57:19 -0600

    Added documentation to Floor, Enemy Nav, and Patrol scripts. ADDED: Patrol script to allow AI to patrol between points on BattleRoom1

--- thisprojectneedsaname/Assets/Scripts/Floor/Floor.cs
+++ thisprojectneedsaname/Assets/Scripts/Floor/Floor.cs
@@ -4,33 +4,53 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
-    public GameObject parent;
-    public GameObject Enemy;
-    public GameObject EnemySpawn1;
-    public GameObject EnemySpawn2;
-    public GameObject EnemySpawn3;
-    public GameObject EnemySpawn4;
-    public GameObject EnemySpawn5;
-    public GameObject EnemySpawn6;
-    private GameObject Enemy1;
-    private GameObject Enemy2;
-    private GameObject Enemy3;
-    private GameObject Enemy4;
-    private GameObject Enemy5;
-    private GameObject Enemy6;
-    public GameObject Target;
-    // Start is called before the first frame update
+    public GameObject parent; /// Parent GameObject.
+    public GameObject Enemy; /// Enemy prefab.
+    public GameObject EnemySpawn1; /// Spawn point for enemy clone 1.
+    public GameObject EnemySpawn2; /// Spawn point for enemy clone 2.
+    public GameObject EnemySpawn3; /// Spawn point for enemy clone 3.
+    public GameObject EnemySpawn4; /// Spawn point for enemy clone 4.
+    public GameObject EnemySpawn5; /// Spawn point for enemy clone 5.
+    public GameObject EnemySpawn6; /// Spawn point for enemy clone 6.
+    private GameObject Enemy1; /// Enemy clone 1.
+    private GameObject Enemy2; /// Enemy clone 2.
+    private GameObject Enemy3; /// Enemy clone 3.
+    private GameObject Enemy4; /// Enemy clone 4.
+    private GameObject Enemy5; /// Enemy clone 5.
+    private GameObject Enemy6; /// Enemy clone 6.
+    public GameObject Target; /// GameObject Target.
+
+
+    /**
+      *@pre Called when the game is launched and before update.
+      *@post None.
+      *@param None.
+      *@return None.
+      */
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
+    /**
+      *@pre Called everyframe after the game launches.
+      *@post None.
+      *@param None.
+      *@return None.
+      */
     void Update()
     {
         
     }
 
+
+    /**
+      *@pre Game must be launched player must enter a BattleRoom.
+      *@post Upon entry into a BattleRoom, 6 Enemies are instantiated (spawned) into that room at their respected spawn points. Each enemy is a clone of the prefab Enemy.
+      *@param None.
+      *@return None.
+      */
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
@@ -44,6 +64,13 @@ public class Floor : MonoBehaviour
         }
     }
 
+
+    /**
+      *@pre Game must be launched. Player must exit a BattleRoom.
+      *@post All the instantiated enemies get destroyed and are no longer visible in game or scene.
+      *@param None.
+      *@return None.
+      */
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
